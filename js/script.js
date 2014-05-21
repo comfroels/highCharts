b71059da3e4b08539ccf9c951b6762e05fda4efa
Nathan White <comfroels@gmail.com>
2014-05-20 20:12:30 -0600

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
         
+		$('table').tablesorter();
+        
         $('#myChart').highcharts({
 	            
 	            chart: {
@@ -51,9 +53,7 @@ $(document).ready(function () {
 	            credits: {
 	                enabled: false
 	            },
-	            // series: [{
-	                
-	            // }]
+	            
 	        });
 
 
@@ -63,21 +63,25 @@ $(document).ready(function () {
 			vals.push(parseFloat($('#php').val()));
 			vals.push(parseFloat($('#ruby').val()));
 			vals.push(parseFloat($('#javascript').val()));
-			console.log(vals);
-			
-        	
+			        	
         	var chart = $('#myChart').highcharts();
 
         	chart.addSeries({
         		name: yourName,
         		data: vals
         	});
+	        
+        	var myphp = parseFloat($('#php').val());
+        	var myruby = parseFloat($('#ruby').val());
+        	var myjavascript = parseFloat($('#javascript').val());
 
-        	
+	        $('#tableBody').append('<tr><td>'+yourName+'</td><td>PHP</td><td>'+myphp+'</td></tr>');
+	        $('#tableBody').append('<tr><td>'+yourName+'</td><td>Ruby</td><td>'+myruby+'</td></tr>');
+	        $('#tableBody').append('<tr><td>'+yourName+'</td><td>Javascript</td><td>'+myjavascript+'</td></tr>');
 
-        	
-
+	        $('table').trigger("update");
 	        return false;
 			
 		});
+
     });
